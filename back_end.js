7734ff0adb32458c2298585792a82545818a0458
Phoebe <meipbe@gmail.com>
2017-09-22 15:17:13 -0500

    Created object to hold functions: tweetCount, displayTweet

--- back_end.js
+++ back_end.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-  const $body = $('body');
-  $body.html('');
+//  const $body = $('body');
+//  const $tweetBody = $('body.div.tweets');
+//  $body.html('');
   
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    const tweet = streams.home[index];
-    const $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at.toLocaleTimeString());
-    $tweet.appendTo($body);
-    index--;
+//  let index = streams.home.length - 1;
+//  while(index >= 0){
+//    const tweet = streams.home[index];
+//    const $tweet = $('<div></div>');
+//    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at.toLocaleTimeString());
+//    $tweet.appendTo($body);
+//    index--;
+
+  
+  const tweetHandlers = {
+  
+    tweetCount: function() {return streams.home.length;},
+
+    displayTweet: function() {
+      const tweet = streams.home[0];
+      const $tweet = $('<div></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at.toLocaleTimeString());
+      $tweet.appendTo($('.tweets'))
+    }
   }
-});
+})
+
