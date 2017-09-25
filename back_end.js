69e1c33f974961398dbe3f01433ba6118ad12c38
Phoebe <meipbe@gmail.com>
2017-09-24 19:50:15 -0500

    Moved name hyperlink and time into spans contained within a tweet container; created a tweet filter for users

--- back_end.js
+++ back_end.js
@@ -1,32 +1,62 @@
 $(document).ready(function(){
 //  const $body = $('body');
 
-  const $tweetContainer = $('.container.tweets');
+  const $allTweetContainer = $('.container.tweets');
   const $notificationContainer = $('.container.notification');
-  let initialTweetCount = streams.home.length;
+  const homeFeed = streams.home;
+  let initialTweetCount = homeFeed.length;
+  let tweetUsers = streams.users;
   
 
   const tweetEvents = {
     
-    displayIndividualTweet: function(index) {
-      let tweet = streams.home[index];
-      let $tweet = $('<div class="tweets"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at.toLocaleTimeString());
-      $tweet.prependTo($tweetContainer);
+    createTweetHeader: function(tweet) {
+      let $tweetHeader = $('<div class="tweet-header"></div>');
+      let $headerSpan = $('<span class="header-span"></span>');
+      let $timeSpan = $('<span class="time-span"></span>');
+    
+      let time = tweet.created_at.toLocaleTimeString();
+      let userLink = $('<a href="#'+ tweet.user + '">@' + tweet.user + '</a>');
+      
+      $headerSpan.append(userLink);
+      $timeSpan.append(time);
+      
+      $tweetHeader.append($headerSpan);
+      $tweetHeader.append($timeSpan)
+      return $tweetHeader;
     },
     
-    displayTweets: function(start, end) {
+    createTweetBody: function(tweet) {
+      let $tweetBody = $('<div class="tweet-body"></div>');
+      let message = $('<p>' + tweet.message + ' ' + '</p>');
+      
+      $tweetBody.append(message);
+      return $tweetBody;
+    },
+    
+    displayIndividualTweet: function(collection, index) {
+      let tweet = collection[index];
+      let $tweet = $('<div class="container tweet"></div>');
+      let $tweetHeader = tweetEvents.createTweetHeader(tweet);
+      let $tweetBody = tweetEvents.createTweetBody(tweet);
+      
+      $tweetHeader.appendTo($tweet);
+      $tweetBody.appendTo($tweet);
+      $tweet.prependTo($allTweetContainer);
+    },
+    
+    displayTweets: function(collection, start, end) {
       while (start < end) {
-        tweetEvents.displayIndividualTweet(start);
+        tweetEvents.displayIndividualTweet(collection, start);
         start++;
       }
     },
     
-    countTweets: function() {return streams.home.length},
+    countTweets: function() {return homeFeed.length},
     
     notificationForNewTweets: function(newTweetCount) {
       let tweetMessage = $('<button id="view-tweets"></button>');
-      tweetMessage.text('you have ' + (newTweetCount - initialTweetCount) + '+ new tweets').prependTo($notificationContainer);
+      tweetMessage.text('See ' + (newTweetCount - initialTweetCount) + ' new tweets').prependTo($notificationContainer);
     },
     
     checkNewTweets: function() {
@@ -40,16 +70,22 @@ $(document).ready(function(){
     
   }
   
+  
   const tweetHandlers = {
     
     displayInitialTweets: function() {
-      tweetEvents.displayTweets(0, initialTweetCount - 1);
+      tweetEvents.displayTweets(homeFeed, 0, initialTweetCount - 1);
     },
     
     displayNewTweets: function() {
       let newTweetCount = tweetEvents.countTweets();
-      tweetEvents.displayTweets(initialTweetCount - 1, newTweetCount);
+      tweetEvents.displayTweets(homeFeed, initialTweetCount - 1, newTweetCount);
       initialTweetCount = newTweetCount;
+    }, 
+    
+    filterTweetsByUser: function(user) {
+      let userFeed = tweetUsers[user];
+      tweetEvents.displayTweets(userFeed, 0, userFeed.length - 1);
     }
     
   }
@@ -68,6 +104,7 @@ $(document).ready(function(){
     })
 
   }
-  
+
+
 
 })
